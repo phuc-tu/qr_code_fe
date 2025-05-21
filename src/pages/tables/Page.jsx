848319fceddc83fd100c3ed8a20282f975d122ef
Phuc Tu <nguyenphuctu.0904@gmail.com>
2025-05-22 00:53:12 +0700

    first commit

--- src/pages/tables/Page.jsx
+++ src/pages/tables/Page.jsx
@@ -1,20 +1,27 @@
 import React from "react";
 
 const TablePage = (props) => {
-    return <div className="card bg-base-100 w-96 shadow-sm">
-  <figure>
-    <img
-      src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
-      alt="Shoes" />
-  </figure>
-  <div className="card-body">
-    <h2 className="card-title">Table {props.id}</h2>
-    <p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
-    <div className="card-actions justify-end">
-      <button className="btn btn-primary">Buy Now</button>
-    </div>
-  </div>
-</div>
+    return (
+        <div className="card bg-base-100 w-96 shadow-sm">
+            <figure>
+                <img
+                    src="https://media.istockphoto.com/id/828088276/vector/qr-code-illustration.jpg?s=612x612&w=0&k=20&c=FnA7agr57XpFi081ZT5sEmxhLytMBlK4vzdQxt8A70M="
+                    alt="Shoes"
+          
+                />
+            </figure>
+            <div className="card-body">
+                <h2 className="card-title">Table {props.id}</h2>
+                <p>
+                    A card component has a figure, a body part, and inside body
+                    there are title and actions parts
+                </p>
+                <div className="card-actions justify-end">
+                    <button className="btn btn-primary">View Now</button>
+                </div>
+            </div>
+        </div>
+    );
 };
 
 export default TablePage;
