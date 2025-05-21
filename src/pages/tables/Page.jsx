5be557eeb7d0111a93ac57c0d0f9da0d59586fa8
Phuc Tu <nguyenphuctu.0904@gmail.com>
2025-05-22 00:46:57 +0700

    .

--- src/pages/tables/Page.jsx
+++ src/pages/tables/Page.jsx
@@ -8,7 +8,7 @@ const TablePage = (props) => {
       alt="Shoes" />
   </figure>
   <div className="card-body">
-    <h2 className="card-title">Card Title {props.id}</h2>
+    <h2 className="card-title">Table {props.id}</h2>
     <p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
     <div className="card-actions justify-end">
       <button className="btn btn-primary">Buy Now</button>
